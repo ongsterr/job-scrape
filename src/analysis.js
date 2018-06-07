b6a50ed5e5b28ad5c2e4ac9e6ea391624ae50cab
ongsterr <ong.chris11@gmail.com>
2018-06-07 22:54:56 +1000

    Add analysis on companies and skills required

--- src/analysis.js
+++ src/analysis.js
@@ -1,3 +1,9 @@
+/* Questions
+Where in Australia can i find junior/grad dev opportunities?
+What are the top companies hiring at the moment?
+What are some key skills required by hiring company?
+*/
+
 const mongoose = require('mongoose');
 const Job = require('./models/job');
 
@@ -6,6 +12,7 @@ mongoose.connect('mongodb://localhost/joblist', function (err) {
     console.log('Successfully connected');
 });
 
+// Queries
 async function getLocation() {
 
     let query1 = await Job.find().select('location')
@@ -15,11 +22,11 @@ async function getLocation() {
     }
     const locationArr = uniq(distLocation);
     
-    let query2 = Job.find().select('location')
-    let totalCount = query1.length
-    const locationObj = []
+    let query2 = Job.find().select('location');
+    let totalCount = query1.length;
+    const locationObj = [];
     for (let location of locationArr) {
-        const filter = await query2.where({location: location})
+        const filter = await query2.where({location: location});
         locationObj.push({
             city: location,
             count: filter.length,
@@ -29,6 +36,54 @@ async function getLocation() {
     return locationObj
 }
 
+async function getCompanies() {
+
+    let query1 = await Job.find().select('company');
+    const distCompany = [];
+    for (let i = 0; i <= (query1.length - 1); i++) {
+        distCompany.push(query1[i].company);
+    }
+    const companyArr = uniq(distCompany);
+
+    let query2 = Job.find().select('company');
+    let totalCount = query1.length;
+    const companyObj = [];
+    for (let company of companyArr) {
+        const filter = await query2.where({ company: company });
+        const count = filter.length;
+        companyObj.push({
+            company: company,
+            count: count,
+            ratio: (count / totalCount).toFixed(3) * 1
+        })
+    }
+    return companyObj
+}
+
+async function getSkills() {
+    let query1 = await Job.find().select('type');
+    let skillsNestArr = [];
+    for (let query of query1) {
+        skillsNestArr.push(query.type);
+    }
+
+    let skillsArr = skillsNestArr.reduce((a, b) => a.concat(b), []);
+    let distSkills = uniq(skillsArr);
+
+    let totalCount = query1.length
+    const skillsObj = [];
+    for (let skill of distSkills) {
+        const count = skillsArr.filter(type => type === skill).length
+        skillsObj.push({
+            skill: skill,
+            count: count,
+            ratio: (count/totalCount).toFixed(3) * 1
+        })
+    }
+    return skillsObj
+}
+
+// Supporting functions
 function uniq(a) {
     const array = a.sort().filter((item, pos, arr) => {
         return !pos || item != arr[pos - 1];
@@ -36,5 +91,12 @@ function uniq(a) {
     return array;
 }
 
+// Query Execution
 getLocation()
     .then( a => console.log(a) )
+
+getCompanies()
+    .then(a => console.log(a))
+
+getSkills()
+    .then(a => console.log(a))
